aa71f973935ac604c0943726901cae8b4390d18b
ParkJeongHwan <Lariat@naver.com>
2017-04-21 10:36:30 +0900

    행성 충돌 업데이트

---
+++ SpaceShooters/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);               // Debug문
+
+        if (other.tag == "Boundary")         //  충돌체 중 Boundary를 제외
+        {
+            return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ SpaceShooters/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+    Rigidbody rd;
+	// Use this for initialization
+	void Start () {
+        rd = GetComponent<Rigidbody>();
+        rd.angularVelocity = Random.insideUnitSphere * tumble;        
+    }
+	
+}
